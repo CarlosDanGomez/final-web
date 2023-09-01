d3979339c96c15c4cf0ece19ab91a2a827806934
Daniel <carlos_dan.gomez@uao.edu.co>
2023-08-31 20:28:57 -0500

    Dios queriendo lo ultimo que subo hoy
    Por favor señor ten piedad de mi

---
+++ src/Pages/CargarProducto/CargarProducto.jsx
@@ -0,0 +1,12 @@
+import './Tienda.css'
+import Menu from '../../Components/Shared/Menu';
+import Producto from '../../Components/Shared/Producto';
+
+function CargarProducto () {
+<>
+<Menu/>
+<Producto/>
+</>
+}
+
+export default CargarProducto;
